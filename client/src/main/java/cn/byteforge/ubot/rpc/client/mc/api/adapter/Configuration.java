7f1172c8cc56881bdb48a4cef4991a771d1648e8
IllTamer <mail@illtamer.com>
2024-07-06 21:37:11 +0800

    feat: 新增抽象层

---
+++ client/src/main/java/cn/byteforge/ubot/rpc/client/mc/api/adapter/Configuration.java
@@ -0,0 +1,35 @@
+package cn.byteforge.ubot.rpc.client.mc.api.adapter;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public interface Configuration extends ConfigSection {
+
+    @Nullable
+    ConfigSection getSection(String path);
+
+    ConfigSection createSection(String path, Map<String, Object> data);
+
+    /**
+     * 序列化
+     * */
+    String saveToString();
+
+    /**
+     * 将缓存数据保存到磁盘
+     * */
+    void save(File file) throws IOException;
+
+    /**
+     * 将磁盘文件加载到缓存
+     * */
+    void load(File file) throws IOException;
+
+    /**
+     * 从字符串加载
+     * */
+    void load(String yaml);
+
+}
