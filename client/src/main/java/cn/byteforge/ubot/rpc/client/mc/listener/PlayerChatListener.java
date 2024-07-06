3347388650327d1a4687f9e1b4e6710a2d9f10a8
IllTamer <mail@illtamer.com>
2024-07-06 21:41:12 +0800

    feat: 调整游戏监听实现

--- client/src/main/java/cn/byteforge/ubot/rpc/client/mc/listener/PlayerChatListener.java
+++ client/src/main/java/cn/byteforge/ubot/rpc/client/mc/listener/PlayerChatListener.java
@@ -1,5 +1,7 @@
 package cn.byteforge.ubot.rpc.client.mc.listener;
 
+import cn.byteforge.ubot.rpc.client.mc.api.Scheduler;
+import cn.byteforge.ubot.rpc.client.mc.script.ScriptHelper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -8,7 +10,8 @@ public class PlayerChatListener implements Listener {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-
+        Scheduler.runTask(() ->
+                ScriptHelper.broadcastEvent(event));
     }
 
 }
