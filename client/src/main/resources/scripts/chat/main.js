9e4fb73af239a9c062d4cdbcf0df870bebd38b57
IllTamer <mail@illtamer.com>
2024-07-06 21:42:41 +0800

    feat: 更新内置js插件

--- client/src/main/resources/scripts/chat/main.js
+++ client/src/main/resources/scripts/chat/main.js
@@ -1,13 +1,16 @@
 // 聊天互通附属
 
-// TODO js调java方法
-
 // 监听游戏事件
-function onGameEvent(event) {
+function onGameEvent(event, type) {
     // check if message event
+    if (type !== "AsyncPlayerChatEvent") {
+        return;
+    }
+    var message = event.getMessage();
+    ScriptHelper.sendRPCMessage("JS接收到: " + message);
 }
 
 // 获取脚本版本
 function getVersion() {
-    return "20240630-0"
+    return "20240630-0";
 }
