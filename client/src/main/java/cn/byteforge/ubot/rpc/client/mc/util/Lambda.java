768fe73301fa62e487f038bd1070c5b397a4fecd
IllTamer <mail@illtamer.com>
2024-07-06 21:37:42 +0800

    feat: 新增方法类

---
+++ client/src/main/java/cn/byteforge/ubot/rpc/client/mc/util/Lambda.java
@@ -0,0 +1,27 @@
+package cn.byteforge.ubot.rpc.client.mc.util;
+
+import lombok.experimental.UtilityClass;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
+
+@UtilityClass
+public class Lambda {
+
+    /**
+     * @param func 结果参数函数式调用
+     * @param result 结果参数
+     * */
+    @Nullable
+    public static <Result, Return> Return nullableInvoke(Function<Result, Return> func, @Nullable Result result) {
+        return result != null ? func.apply(result) : null;
+    }
+
+    /**
+     * 三元运算
+     * */
+    public static <Type> Type ternary(boolean condition, Type pass, Type deny) {
+        return condition ? pass : deny;
+    }
+
+}
